8f068d69d00f320e70b0dd09524ae8860406ca65
acobb <aecobb53@gmail.com>
2022-03-09 10:47:24 -0700

    updating rc

--- bin/json_ish_parser.py
+++ bin/json_ish_parser.py
@@ -5,52 +5,56 @@ import uuid
 import argparse
 import re
 
-# from sys import argv
-
-# # print(argv)
-# # print(argv[0])
-# # print(argv[1])
-# # print(argv[2])
-
-
 # Argparse
 aparse = argparse.ArgumentParser(description='Task Warrior TMUX wrapper')
 aparse.add_argument('args', nargs='*')
 args = aparse.parse_args()
 
-# print(args)
-print('')
+def json_ish_parse(string):
+    output1 = [[]]
+    output2 = []
+    indent_length = 4
+    tab = ' ' * indent_length
+    index = 0
+    set_indent = 0
+    current_indent = 0
+    in_quotes = []
 
-output1 = [[]]
-output2 = []
-indent_length = 4
-tab = ' ' * indent_length
-# tab = '----'
-index = 0
-set_indent = 0
-current_indent = 0
+    # for character in args.args[0]:
+    for character in string:
+        if character in [')', ']', '}']:
+            index += 1
+            set_indent -= 1
+            current_indent = set_indent
+            output1.append([])
+        if len(output1[index]) == 0:
+            output1[index].append(current_indent * tab)
+        if character == ' ' and not in_quotes:
+            continue
+        output1[index].append(character)
+        if character == ',':
+            index += 1
+            output1.append([])
+        if character in ['(', '[', '{']:
+            index += 1
+            set_indent += 1
+            current_indent = set_indent
+            output1.append([])
+        if character in ['"', "'"]:
+            if not in_quotes:
+                in_quotes.append(character)
+            else:
+                if character == in_quotes[-1]:
+                    in_quotes.pop(-1)
+                else:
+                    in_quotes.append(character)
 
-for c in args.args[0]:
-    if c in [')', ']', '}']:
-        index += 1
-        set_indent -= 1
-        current_indent = set_indent
-        output1.append([])
-    if len(output1[index]) == 0:
-        output1[index].append(current_indent * tab)
-    output1[index].append(c)
-    if c == ',':
-        index += 1
-        output1.append([])
-    if c in ['(', '[', '{']:
-        index += 1
-        set_indent += 1
-        current_indent = set_indent
-        output1.append([])
+    for l in output1:
+        if l != []:
+            output2.append(''.join(l))
+    return output2
 
-for l in output1:
-    if l != []:
-        output2.append(''.join(l))
+output2 = json_ish_parse(' '.join(args.args))
 
 for l in output2:
     print(l)
